0199ec6931a70a9d2f668d509df6a6393df8fed4
humbl3LilaS <sabishinekobebe@gmail.com>
2025-05-06 18:49:51 +0630

    config: prettier and eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,36 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        rules: {
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    args: "all",
+                    argsIgnorePattern: "^_",
+                    caughtErrors: "all",
+                    caughtErrorsIgnorePattern: "^_",
+                    destructuredArrayIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    ignoreRestSiblings: true,
+                },
+            ],
+            quotes: [
+                "error",
+                "double",
+                {
+                    allowTemplateLiterals: true,
+                },
+            ],
+            semi: ["error", "always"],
+            "comma-dangle": ["error", "always-multiline"],
+        },
+    },
 ];
 
 export default eslintConfig;
