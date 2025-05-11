32b0324c1ac0bfe4b3dfd6d92cead14f9671da05
humbl3LilaS <sabishinekobebe@gmail.com>
2025-05-11 21:16:59 +0630

    fix: budget schema to include currency

--- drizzle/0003_long_major_mapleleaf.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "budget" RENAME COLUMN "durationFrom" TO "duration_from";--> statement-breakpoint
-ALTER TABLE "budget" RENAME COLUMN "durationTo" TO "duration_to";
--- drizzle/0006_cultured_hellion.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "user" ALTER COLUMN "user_name" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "user" ALTER COLUMN "password" SET NOT NULL;
