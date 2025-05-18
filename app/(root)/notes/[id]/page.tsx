6e9b780cecb67ab4fa16075679c11f806b95e2aa
NaingMinThant77 <naingminthant07@gmail.com>
2025-05-18 16:59:51 +0630

    ConflictSolved-3

--- app/(root)/notes/[id]/page.tsx
+++ app/(root)/notes/[id]/page.tsx
@@ -1,26 +1,9 @@
-import { getNoteContentById, updateNote } from "@/features/notes/actions";
 import NoteEditor from "@/features/notes/components/note-editor";
-import { INoteType } from "@/features/notes/components/note-types";
-// import { getUserSession } from "@/lib/server-util";
 
-export default async function NotePage({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const id = (await params).id;
-  // const session = await getUserSession();
-  const data = await getNoteContentById(id);
-
-  async function mutateNote(data: INoteType) {
-    "use server";
-    await updateNote(id, data.body || "", data.title);
-    // console.log("SAVED!!!");
-  }
-
+export default function page() {
   return (
     <section className="max-w-7xl mx-auto">
-      <NoteEditor data={data} mutate={mutateNote} />
+      <NoteEditor />
     </section>
   );
 }
--- features/notes/components/note-editor.tsx
+++ features/notes/components/note-editor.tsx
@@ -1,28 +1,20 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { INoteType, TNoteType } from "./note-types";
 const Editor = dynamic(() => import("@/features/notes/components/Editor"), {
   ssr: false,
 });
 
-export default function NoteEditor({
-  data,
-  mutate,
-}: {
-  data: TNoteType;
-  mutate: (data: INoteType) => Promise<void>;
-}) {
+export default function NoteEditor() {
   return (
     <div className="min-h-[calc(100dvh-80px)]">
       <Editor
-        data={data}
-        mutate={mutate}
-        // id={id}
-        // title={title}
-        // body={body}
-        // createdAt={createdAt}
-        // updatedAt={updatedAt}
+        title="Note-1"
+        body={
+          "# Header-1 \nlorem ipsum `code` dolor sit amet.\n```js\nconsole.log('hello world') \n```\n \n\n## Sub Header \n > quote text\n\n[google](https://www.google.com) \n\n Hello world\n\n "
+        }
+        createdAt={new Date("12 3 2020")}
+        updatedAt={new Date("12 3 2020")}
       />
     </div>
   );
