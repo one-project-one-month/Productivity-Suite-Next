065388dbcb7aac74578429571fabcf3f9aaaf491
humbl3LilaS <sabishinekobebe@gmail.com>
2025-05-22 19:36:11 +0630

    add: Auth logic in /budget-tracker routes

--- app/(root)/budget-tracker/layout.tsx
+++ app/(root)/budget-tracker/layout.tsx
@@ -1,7 +1,13 @@
 import { ReactNode } from "react";
 import BudgetTrackerNav from "@/features/budget-tracker/components/budget-tracker-nav";
+import { getUserSession } from "@/lib/server-util";
+import { redirect } from "next/navigation";
 
-const BudgetTrackerLayout = ({ children }: { children: ReactNode }) => {
+const BudgetTrackerLayout = async ({ children }: { children: ReactNode }) => {
+  const session = await getUserSession();
+  if (!session) {
+    return redirect("/auth/sign-in");
+  }
   return (
     <main>
       <div className={"px-4 py-6 md:px-6 lg:px-20"}>
