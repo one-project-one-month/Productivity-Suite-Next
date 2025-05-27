6cb4d4fa9a327ce844b72d547fba9ab5ec387c5e
humbl3LilaS <sabishinekobebe@gmail.com>
2025-05-27 21:03:21 +0630

    fix: category schema & delete userCategory table

---
+++ drizzle/0013_amusing_punisher.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "category" ADD COLUMN "user_id" text;--> statement-breakpoint
+ALTER TABLE "category" ADD CONSTRAINT "category_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
